2b38423506dbf4ef9631016a6aa3d85131dac52b
Ken Peterson <kenpeterson3@gmail.com>
2017-09-01 13:20:39 -0400

    pair exercise 1 make change Bobby and Ken

--- m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MakeChange.java
+++ m1-w1d5-command-line-programs-exercises-pair/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then display the change required.
  
@@ -9,9 +12,31 @@ package com.techelevator;
  The change required is 76.35
  */
 public class MakeChange {
+	
 
 	public static void main(String[] args) {
-
+		
+		DecimalFormat twoDecimals = new DecimalFormat("0.00");
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the amount billed: ");
+		
+		double amountBilled = input.nextDouble();
+		
+		System.out.print("Enter the amount tendered: ");
+		
+		double amountTendered = input.nextDouble();
+		
+		System.out.print(twoDecimals.format(changeRequired(amountBilled,amountTendered)));
+		
+		
 	}
-
+	
+	public static double changeRequired (double amountBilled, double amountTendered) {
+		
+		double changeReturned = (amountTendered - amountBilled);
+		return (changeReturned);
+		
+	}
 }
